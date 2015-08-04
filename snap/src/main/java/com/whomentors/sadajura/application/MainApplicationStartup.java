c1e26adad8563a6d80eac4d26d6519f73764f70c
Michael Y. Huh <mhuh@yoonhuh.com>
2015-08-04 13:50:10 -0700

    * Updated application configuration.

---
+++ snap/src/main/java/com/whomentors/sadajura/application/MainApplicationStartup.java
@@ -0,0 +1,21 @@
+package com.whomentors.sadajura.application;
+
+import android.app.Application;
+
+import com.whomentors.sadajura.activities.SJMainActivity;
+import com.parse.Parse;
+import com.parse.PushService;
+
+public class MainApplicationStartup extends Application {
+
+    private static final String PARSE_ID = "NaN";
+    private static final String PARSE_KEY = "NaN";
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        Parse.initialize(this, PARSE_ID, PARSE_KEY);
+
+        PushService.setDefaultPushCallback(this, SJMainActivity.class);
+    }
+}
